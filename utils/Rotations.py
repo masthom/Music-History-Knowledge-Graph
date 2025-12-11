0a7248f451a29a542b64689a1d074b8f9115a958
thomas.ahrend@unibas.ch <thomas.ahrend@unibas.ch>
2025-12-11 10:18:54 +0100

    add Herschkowitz, Kammersuite

--- utils/Rotations.py
+++ utils/Rotations.py
@@ -107,7 +107,7 @@ def print_report(data: dict):
 if __name__ == "__main__":
     # === Beispiel-Input ===
     # rowClass der Rotation 1 (Unterstrich-getrennt)
-    input_pattern = "1_6_10_1_5"   # ersetze durch dein Muster
+    input_pattern = "1_4_11_7_9_1_5_1_4_11_4"   # ersetze durch dein Muster
 
     # Modulus (standard 12 für Pitch Classes). Ändere, wenn du in anderem Modul arbeiten willst.
     mod = 12
